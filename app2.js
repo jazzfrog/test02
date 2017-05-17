ce4ef3e190c399ffc6efa1da5c9fe9446a4446e6
jazzfrog <swha189@gmail.com>
2017-05-17 12:39:15 +0900

    BBS simple Test 170517

--- app2.js
+++ app2.js
@@ -3,6 +3,7 @@ var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 mongoose.connect(process.env.MONGO_DB);
 
@@ -21,7 +22,15 @@ var dataSchema = mongoose.Schema({
   count:Number
 });
 
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
+});
+
 var Data = mongoose.model('data',dataSchema);
+var Post = mongoose.model('post',postSchema);
 
 Data.findOne( {name:"myData"}, function(err, data){
   if(err) return console.log("Data Error:",err);
@@ -48,6 +57,8 @@ app.set("view engine",'ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 // var data={count:0};
 
@@ -59,7 +70,7 @@ app.get("/",function(req, res) {
 
 
   Data.findOne({name:"myData"}, function(err,data){
-    if(err) return console.log("Datta error: ",err);
+    if(err) return console.log("Data error: ",err);
     data.count++;
     data.save(function(err){
       if(err) return console.log("data save err: ", err);
@@ -92,6 +103,77 @@ app.get("/set/:num",function(req, res) {
   else getCnt(res);
   // res.render('my_ejs',data);
 });
+
+
+app.get("/posts",function(req, res) {
+  // Post.find({}, function(err,posts){
+  Post.find({}).sort('-createdAt').exec( function(err,posts){
+    if(err) return res.json({success:false, message:err});
+    //res.json({success:true, data:posts});
+
+    Data.findOne({name:"myData"}, function(err,data){
+      if(err) return console.log("Data error: ",err);
+      data.count++;
+      data.save(function(err){
+        if(err) return console.log("data save err: ", err);
+        //res.render('footer',data);
+        res.render("posts/index",{data:posts, cntt:data});
+      });
+    });
+
+    // res.render("posts/index",{data:posts, cntt:55});
+  });
+
+});
+
+app.get("/posts/new",function(req, res) {
+  res.render("posts/new");
+});
+
+app.post("/posts",function(req, res) {
+  Post.create(req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, data:post});
+    res.redirect("/posts");
+  });
+});
+
+app.get("/posts/:id",function(req, res) {
+  Post.findById(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, data:post});
+    res.render("posts/show",{data:post});
+  });
+});
+
+app.get("/posts/:id/edit",function(req, res) {
+  Post.findById(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, data:post});
+    res.render("posts/edit",{data:post});
+  });
+});
+
+app.put("/posts/:id",function(req, res) {
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, message:post._id+" updated"});
+    res.redirect("/posts/"+req.params.id);
+  });
+});
+
+app.delete("/posts/:id",function(req, res) {
+  // req.body.post.updatedAt=Date.now();
+  Post.findByIdAndRemove(req.params.id,  function(err,post){
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, message:post._id+" deleted"});
+    res.redirect("/posts");
+  });
+});
+
+
+
 console.log(__dirname);
 
 
@@ -124,5 +206,5 @@ function setCnt(res, num){
 }
 
 app.listen(3002, function() {
-  console.log('Server is ejs On....!');
+  console.log('===HAHA===> Server is ejs On....!');
 });
